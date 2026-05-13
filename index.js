6fe8be6efa17ee588815df10677ea8d4a2087a97
Amirul <amirulislam9.e@gmail.com>
2026-05-14 00:17:55 +0600

    catch new destination  data from frontend and keep it in mongodb  database

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
 // Load environment variables from .env
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express();
 const port = process.env.PORT;
 const uri = process.env.MONGO_URI;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +22,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // "wanderlust" নামে database select করা হয়েছে
+    const database = client.db("wanderlustdb");
+
+    //  "destination" collection select করা হয়েছে
+    const destinationCollection = database.collection("destination");
+
+    // add a new destination
+    app.post("/add-destination", async (req, res) => {
+      const newDestination = req.body;
+      const result = await destinationCollection.insertOne(newDestination);
+      console.log(result);
+      // Express.js ব্যাকএন্ড হলে:
+      res.status(200).json({ message: "new destination added" });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
